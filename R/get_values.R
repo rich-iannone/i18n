f22b2df4441c15a01848b90c46d9c6fb18523120
Richard Iannone <riannone@me.com>
2022-03-16 14:32:15 -0400

    Update get_values.R

--- R/get_values.R
+++ R/get_values.R
@@ -2,10 +2,9 @@
 #'
 #' @description
 #' The `currencies` table contains information of currency codes and localized
-#' display names and symbols across 571 locales. The `get_currency_value()`
-#' function allows one to extract a single parameter value from the table by
-#' supplying the `locale`, the currency code, and one of the following parameter
-#' names.
+#' display names and symbols across 571 locales. The `cldr_currency()` function
+#' allows one to extract a single parameter value from the table by supplying
+#' the `locale`, the currency code, and one of the following parameter names:
 #' 
 #' - `"sym"`: corresponds to the `currency_symbol` column in `currencies`
 #' - `"sym_narrow"`: is the `currency_symbol_narrow` column in `currencies`
@@ -16,7 +15,7 @@
 #' @return A length one character vector.
 #' 
 #' @export
-get_currency_value <- function(
+cldr_currency <- function(
     locale = "en",
     currency = "USD",
     param = c("sym", "sym_narrow", "displ_name", "displ_ct_1", "displ_ct_oth")
@@ -38,4 +37,220 @@ get_currency_value <- function(
     currencies$locale == locale &
       currencies$currency_code == currency,
   ][[param_full]]
+}
+
+
+#' Get a single localized value from the `currencies` dataset
+#'
+#' @description
+#' The `locale_names` table contains information on how to express components of
+#' locale codes and this is localized across 571 locales. The `cldr_locale()`
+#' function allows one to extract a named list using a `locale` and one of the
+#' following parameter names:
+#' 
+#' - `"langs"`: corresponds to the `lang_names` column in `locale_names`
+#' - `"scripts"`: is the `script_names` column in `locale_names`
+#' - `"territories"`: is `territory_names`
+#' - `"variants"`: is `variant_names`
+#' 
+#' @return A named list.
+#' 
+#' @export
+cldr_locale <- function(
+    locale = "en",
+    param = c("langs", "scripts", "territories", "variants")
+) {
+  
+  param <- match.arg(param)
+  
+  param_full <-
+    switch(
+      param,
+      langs = "lang_names",
+      scripts = "script_names",
+      territories = "territory_names",
+      variants = "variant_names"
+    )
+  
+  values <- locale_names[locale_names$locale == locale, ][[param_full]]
+  values <- unlist(values, use.names = TRUE)
+  
+  names(values) <- gsub("^name\\.", "", names(values))
+  
+  as.list(values)
+}
+
+#' @export
+dates_params <-
+  list(
+    months_format_abbrev = "months_format_abbrev",
+    months_format_narrow = "months_format_narrow", 
+    months_format_wide = "months_format_wide",
+    days_standalone_narrow = "days_standalone_narrow", 
+    days_standalone_short = "days_standalone_short",
+    days_standalone_wide = "days_standalone_wide", 
+    quarters_format_abbrev = "quarters_format_abbrev",
+    quarters_format_narrow = "quarters_format_narrow", 
+    quarters_format_wide = "quarters_format_wide",
+    quarters_standalone_abbrev = "quarters_standalone_abbrev", 
+    quarters_standalone_narrow = "quarters_standalone_narrow", 
+    quarters_standalone_wide = "quarters_standalone_wide",
+    dayperiods_format_abbrev = "dayperiods_format_abbrev", 
+    dayperiods_format_narrow = "dayperiods_format_narrow",
+    dayperiods_format_wide = "dayperiods_format_wide", 
+    dayperiods_standalone_abbrev = "dayperiods_standalone_abbrev", 
+    dayperiods_standalone_narrow = "dayperiods_standalone_narrow", 
+    dayperiods_standalone_wide = "dayperiods_standalone_wide", 
+    eras_abbrev = "eras_abbrev",
+    eras_names = "eras_names",
+    eras_narrow = "eras_narrow", 
+    date_formats = "date_formats",
+    date_skeletons = "date_skeletons", 
+    time_formats = "time_formats",
+    time_skeletons = "time_skeletons", 
+    date_time_available_formats = "date_time_available_formats", 
+    date_time_append_items = "date_time_append_items",
+    date_time_interval_formats = "date_time_interval_formats"
+  )
+
+#' Get a single localized value from the `dates` dataset
+#'
+#' @description
+#' The `dates` table contains information on how to express dates and this data
+#' is localized across 571 locales. The `cldr_dates()`
+#' function allows one to extract a named list using a `locale` and a specific
+#' `param`. The `param` values are:
+#' 
+#' - `"locale"`
+#' - `"months_format_abbrev"`
+#' - `"months_format_narrow"`
+#' - `"months_format_wide"`
+#' - `"days_standalone_narrow"`
+#' - `"days_standalone_short"`
+#' - `"days_standalone_wide"`
+#' - `"quarters_format_abbrev"`
+#' - `"quarters_format_narrow"`
+#' - `"quarters_format_wide"`
+#' - `"quarters_standalone_abbrev"`
+#' - `"quarters_standalone_narrow"`
+#' - `"quarters_standalone_wide"`
+#' - `"dayperiods_format_abbrev"`
+#' - `"dayperiods_format_narrow"`
+#' - `"dayperiods_format_wide"`
+#' - `"dayperiods_standalone_abbrev"`
+#' - `"dayperiods_standalone_narrow"`
+#' - `"dayperiods_standalone_wide"`
+#' - `"eras_abbrev"`
+#' - `"eras_names"`
+#' - `"eras_narrow"`
+#' - `"date_formats"`
+#' - `"date_skeletons"`
+#' - `"time_formats"`
+#' - `"time_skeletons"`
+#' - `"date_time_available_formats"`
+#' - `"date_time_append_items"`
+#' - `"date_time_interval_formats"`
+#' 
+#' @return A named list.
+#' 
+#' @export
+cldr_dates <- function(
+    locale = "en",
+    param = dates_params$months_format_abbrev
+) {
+  
+  values <- dates[dates$locale == locale, ][[param]]
+  values <- unlist(values, use.names = TRUE)
+  
+  names(values) <- gsub("^value\\.", "", names(values))
+  
+  as.list(values)
+}
+
+#' @export
+numbers_params <-
+  list(
+    locale = "locale",
+    default_numbering_system = "default_numbering_system", 
+    other_numbering_systems = "other_numbering_systems",
+    minimum_grouping_digits = "minimum_grouping_digits", 
+    decimal = "decimal",
+    group = "group",
+    list = "list",
+    percent_sign = "percent_sign", 
+    plus_sign = "plus_sign",
+    minus_sign = "minus_sign",
+    approx_sign = "approx_sign", 
+    exp_sign = "exp_sign",
+    sup_exp = "sup_exp",
+    per_mille = "per_mille", 
+    infinity = "infinity",
+    nan = "nan",
+    time_sep = "time_sep", 
+    approx_pattern = "approx_pattern",
+    at_least_pattern = "at_least_pattern", 
+    at_most_pattern = "at_most_pattern",
+    range_pattern = "range_pattern", 
+    decimal_format = "decimal_format",
+    sci_format = "sci_format", 
+    percent_format = "percent_format",
+    currency_format = "currency_format", 
+    accounting_format = "accounting_format"
+  )
+
+#' @export
+cldr_numbers <- function(
+    locale = "en",
+    param = numbers_params$default_numbering_system
+) {
+  
+  values <- numbers[numbers$locale == locale, ][[param]]
+  
+  if (is.list(values)) {
+    
+    values <- unlist(values, use.names = TRUE)
+    
+    names(values) <- gsub("^value\\.", "", names(values))
+    
+    values <- as.list(values)
+  }
+  
+  values
+}
+
+#' Get localized values from the `character_labels` dataset
+#'
+#' @description
+#' The `character_labels` table contains information on character patterns
+#' and character labels across 571 locales. The `cldr_character_labels()`
+#' function allows one to extract parameter values from the table by
+#' supplying the `locale` and one of the following parameter names:
+#' 
+#' - `"patterns"`: corresponds to the `character_label_patterns` column in
+#' `character_labels`
+#' - `"labels"`: is the `character_labels` column in `character_labels`
+#' 
+#' @return A named list.
+#' 
+#' @export
+cldr_character_labels <- function(
+    locale = "en",
+    param = c("patterns", "labels")
+) {
+  
+  param <- match.arg(param)
+  
+  param_full <-
+    switch(
+      param,
+      patterns = "character_label_patterns",
+      labels = "character_labels"
+    )
+  
+  values <- character_labels[character_labels$locale == locale,][[param_full]]
+  values <- unlist(values, use.names = TRUE)
+  
+  names(values) <- gsub("^value\\.", "", names(values))
+  
+  as.list(values)
 }
