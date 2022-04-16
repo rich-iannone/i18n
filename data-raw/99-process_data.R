b2f36227c8cbf8e1c18244a43e0e0ec09798b4bf
Richard Iannone <riannone@me.com>
2022-04-16 16:05:48 -0400

    Update 99-process_data.R

--- data-raw/99-process_data.R
+++ data-raw/99-process_data.R
@@ -12,6 +12,8 @@ source("data-raw/06-currencies.R")
 source("data-raw/07-character_labels.R")
 source("data-raw/08-characters.R")
 source("data-raw/09-units.R")
+source("data-raw/10-script_meta.R")
+source("data-raw/11-tz_names.R")
 
 all_locales <- readRDS("data-raw/all_locales.rds")
 all_currency_codes <- readRDS("data-raw/all_currency_codes.rds")
@@ -24,6 +26,7 @@ character_labels <- readRDS("data-raw/character_labels.rds")
 characters <- readRDS("data-raw/characters.rds")
 units <- readRDS("data-raw/units.rds")
 script_metadata <- readRDS("data-raw/script_metadata.rds")
+tz_names <- readRDS("data-raw/tz_names.rds")
 
 # Create external datasets
 usethis::use_data(
@@ -38,6 +41,7 @@ usethis::use_data(
   characters,
   units,
   script_metadata,
+  tz_names,
   internal = FALSE, overwrite = TRUE
 )
 
