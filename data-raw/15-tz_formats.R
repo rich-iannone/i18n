c2260efcef99f268b0c64c0fede1dc9bfdc8369c
Richard Iannone <riannone@me.com>
2022-04-20 00:58:58 -0400

    Update 15-tz_formats.R

--- data-raw/15-tz_formats.R
+++ data-raw/15-tz_formats.R
@@ -10,7 +10,7 @@ tz_formats_tbl <- dplyr::tibble()
 
 for (i in seq_along(all_locales)) {
   
-  tz_names <- 
+  tz_formats <- 
     file.path(
       "https://raw.githubusercontent.com/unicode-org/cldr-json",
       version_tag, "cldr-json/cldr-dates-full/main", 
@@ -18,22 +18,22 @@ for (i in seq_along(all_locales)) {
       "timeZoneNames.json"
     )
   
-  tz_names_data <-
+  tz_formats_data <-
     jsonlite::fromJSON(
-      tz_names,
+      tz_formats,
       flatten = TRUE,
       simplifyDataFrame = TRUE
     )
   
-  zone_names_all <- tz_names_data$main[[1]]$dates$timeZoneNames
+  tz_formats_all <- tz_formats_data$main[[1]]$dates$timeZoneNames
   
-  hour_format <- zone_names_all$hourFormat
-  gmt_format <- zone_names_all$gmtFormat
-  gmt_zero_format <- zone_names_all$gmtZeroFormat
-  region_format <- zone_names_all$regionFormat
-  region_format_daylight <- zone_names_all$`regionFormat-type-daylight`
-  region_format_standard <- zone_names_all$`regionFormat-type-standard`
-  region_format_fallback <- zone_names_all$fallbackFormat
+  hour_format <- tz_formats_all$hourFormat
+  gmt_format <- tz_formats_all$gmtFormat
+  gmt_zero_format <- tz_formats_all$gmtZeroFormat
+  region_format <- tz_formats_all$regionFormat
+  region_format_daylight <- tz_formats_all$`regionFormat-type-daylight`
+  region_format_standard <- tz_formats_all$`regionFormat-type-standard`
+  region_format_fallback <- tz_formats_all$fallbackFormat
   
   tz_formats_tbl_row_i <-
     dplyr::tibble(
