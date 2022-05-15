94f09228e12d658accd5e4e9971016b1b465afd0
Richard Iannone <riannone@me.com>
2022-05-15 16:18:20 -0400

    Update 15-tz_map.R

--- data-raw/15-tz_map.R
+++ data-raw/15-tz_map.R
@@ -5,50 +5,29 @@ library(readr)
 source("data-raw/00-version_tag.R")
 source("data-raw/01-locales.R")
 
-tz_exemplar_tbl <- dplyr::tibble()
+tz_map <- 
+  file.path(
+    "https://raw.githubusercontent.com/unicode-org/cldr-json",
+    version_tag, "cldr-json/cldr-core/supplemental/", 
+    "windowsZones.json"
+  )
+  
+tz_map_data <-
+  jsonlite::fromJSON(
+    tz_map,
+    flatten = TRUE,
+    simplifyDataFrame = TRUE
+  )
+  
+map_data_all <- tz_map_data$supplemental$windowsZones[[1]]
 
-for (i in seq_along(all_locales)) {
-  
-  tz_exemplar <- 
-    file.path(
-      "https://raw.githubusercontent.com/unicode-org/cldr-json",
-      version_tag, "cldr-json/cldr-dates-full/main", 
-      all_locales[i], 
-      "timeZoneNames.json"
-    )
-  
-  tz_exemplar_data <-
-    jsonlite::fromJSON(
-      tz_exemplar,
-      flatten = TRUE,
-      simplifyDataFrame = TRUE
-    )
-  
-  zone_data <- tz_exemplar_data$main[[1]]$dates$timeZoneNames$zone
-  
-  regions <- names(zone_data)
-  
-  exemplar_cities <- c()
-  
-  for (j in seq_along(regions)) {
-    exemplar_cities <- c(exemplar_cities, unlist(zone_data[[j]]))
-  }
-  
-  names(exemplar_cities) <- 
-    gsub(".exemplarCity", "", names(exemplar_cities), fixed = TRUE)
-  
-  tz_exemplar_tbl_row_i <- 
-    tibble::enframe(exemplar_cities) %>%
-    tidyr::pivot_wider(names_from = name) %>%
-    dplyr::mutate(locale = all_locales[i]) %>%
-    dplyr::select(locale, dplyr::everything())
-  
-  # Append row to main table
-  tz_exemplar_tbl <- dplyr::bind_rows(tz_exemplar_tbl, tz_exemplar_tbl_row_i)
-}
+colnames(map_data_all) <- 
+  c("mapzone_other", "mapzone_type", "mapzone_territory")
 
 readr::write_rds(
-  tz_exemplar_tbl,
-  file = "data-raw/tz_exemplar.rds",
+  map_data_all,
+  file = "data-raw/tz_map.rds",
   compress = "xz"
 )
+
+rm(tz_map, tz_map_data, map_data_all)
