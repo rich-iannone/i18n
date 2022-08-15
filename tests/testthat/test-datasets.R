c787d9136dbefae8b082fc30801d2ef3990dc2ba
Richard Iannone <riannone@me.com>
2022-08-15 15:44:20 -0400

    Update test-datasets.R

--- tests/testthat/test-datasets.R
+++ tests/testthat/test-datasets.R
@@ -7,8 +7,18 @@ test_that("Datasets have the expected types", {
   expect_s3_class(dates, "data.frame")
   expect_s3_class(numbers, "data.frame")
   expect_s3_class(currencies, "data.frame")
-  expect_s3_class(character_labels, "data.frame")
   expect_s3_class(characters, "data.frame")
+  expect_s3_class(character_labels, "data.frame")
+  expect_s3_class(delimiters, "data.frame")
+  expect_s3_class(layout, "data.frame")
+  expect_s3_class(script_metadata, "data.frame")
+  expect_s3_class(day_periods, "data.frame")
+  expect_s3_class(tz_exemplar, "data.frame")
+  expect_s3_class(tz_metazone_names, "data.frame")
+  expect_s3_class(tz_metazone_users, "data.frame")
+  expect_s3_class(tz_map, "data.frame")
+  expect_s3_class(tz_formats, "data.frame")
+  expect_s3_class(tz_bcp_id, "data.frame")
 })
 
 test_that("Tabular datasets have the expected dimensions", {
@@ -31,11 +41,35 @@ test_that("Tabular datasets have the expected dimensions", {
   expect_equal(ncol(currencies), 7)
   expect_equal(nrow(currencies), length(all_locales) * length(all_currency_codes))
   
-  expect_equal(ncol(character_labels), 3)
-  expect_equal(nrow(character_labels), length(all_locales))
-  expect_equal(character_labels$locale, all_locales)
-
   expect_equal(ncol(characters), 12)
   expect_equal(nrow(characters), length(all_locales))
   expect_equal(characters$locale, all_locales)
+  
+  expect_equal(ncol(character_labels), 3)
+  expect_equal(nrow(character_labels), length(all_locales))
+  expect_equal(character_labels$locale, all_locales)
+  
+  expect_equal(ncol(delimiters), 5)
+  expect_equal(nrow(delimiters), length(all_locales))
+  
+  expect_equal(ncol(layout), 3)
+  expect_equal(nrow(layout), length(all_locales))
+  
+  expect_equal(ncol(script_metadata), 11)
+
+  expect_equal(ncol(day_periods), 5)
+
+  expect_equal(ncol(tz_exemplar), 443)
+  expect_equal(nrow(tz_exemplar), length(all_locales))
+  
+  expect_equal(ncol(tz_metazone_names), 160)
+  
+  expect_equal(ncol(tz_metazone_users), 4)
+  
+  expect_equal(ncol(tz_map), 3)
+
+  expect_equal(ncol(tz_formats), 8)
+  expect_equal(nrow(tz_formats), length(all_locales))
+
+  expect_equal(ncol(tz_bcp_id), 3)
 })
